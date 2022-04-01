9a2f1b4447d32700fc3c28704a4a03e11fc8df66
johnnymatthews <johnnymatthews@protonmail.com>
2022-04-01 11:36:47 -0400

    Fixes search to work across site.

--- docs/layouts/_default/index.js
+++ docs/layouts/_default/index.js
@@ -1,5 +1,5 @@
 var docs = [
-{{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
+{{ range $index, $page := (where .Site.Pages "Section" "data") -}}
   {
     id: {{ $index }},
     title: "{{ .Title }}",
